20b3aa843217cc6ecf92803f5d923ac61dd58a9d
Josef <juarisco@outlook.com>
2019-11-05 11:34:38 -0500

    Add Codeship status image

--- readme.md
+++ readme.md
@@ -1,1 +1,3 @@
 # Foro de Mr. Juarisco
+
+[![Codeship Status for juarisco/lv-foro-54](https://app.codeship.com/projects/67d45df0-e17a-0137-99be-46db19e429c4/status?branch=master)](https://app.codeship.com/projects/372601)
--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -15,7 +15,7 @@ class ExampleTest extends TestCase
      */
     public function testBasicExample()
     {
-        $this->assertTrue(false);
+        // $this->assertTrue(false);
 
         $user = factory(\App\User::class)->create([
             'name' => 'John Doe',
