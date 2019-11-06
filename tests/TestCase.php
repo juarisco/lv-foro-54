17fee6e14b180d61cc9726bb4dc96846acbc365d
Josef <juarisco@outlook.com>
2019-11-06 16:19:15 -0500

    07.-Creación-de-registros-con-TDD

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\User;
+
 abstract class TestCase extends Illuminate\Foundation\Testing\TestCase
 {
     /**
@@ -10,16 +12,30 @@ abstract class TestCase extends Illuminate\Foundation\Testing\TestCase
     protected $baseUrl = 'http://localhost';
 
     /**
+     * @var \App\User
+     */
+    protected $defaultUser;
+
+    /**
      * Creates the application.
      *
      * @return \Illuminate\Foundation\Application
      */
     public function createApplication()
     {
-        $app = require __DIR__.'/../bootstrap/app.php';
+        $app = require __DIR__ . '/../bootstrap/app.php';
 
         $app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
 
         return $app;
+    }
+
+    public function defaultUser()
+    {
+        if ($this->defaultUser) {
+            return $this->defaultUser;
+        }
+
+        return $this->defaultUser = factory(User::class)->create();
     }
 }
