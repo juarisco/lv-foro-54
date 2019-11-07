4089bb33a52127e6f10489d34085f926c2fbf58d
Josef <juarisco@outlook.com>
2019-11-06 20:15:29 -0500

    10.-Creación-de-métodos-de-prueba-personalizados

--- tests/FeatureTestCase.php
+++ tests/FeatureTestCase.php
@@ -5,4 +5,16 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 class FeatureTestCase extends TestCase
 {
     use DatabaseTransactions;
+
+    function seeErrors(array $fields)
+    {
+        foreach ($fields as $name => $errors) {
+            foreach ((array) $errors as $message) {
+                $this->seeInElement(
+                    "#field_{$name}.has-error .help-block",
+                    $message
+                );
+            }
+        }
+    }
 }
