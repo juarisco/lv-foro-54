93216a2462d149d0e3540bafb3620b7db2f5426c
Josef <juarisco@outlook.com>
2019-11-09 15:31:47 -0500

    13.-Pruebas-de-regresión

--- tests/features/ShowPostTest.php
+++ tests/features/ShowPostTest.php
@@ -15,12 +15,32 @@ class ShowPostTest extends FeatureTestCase
         ]);
 
         $user->posts()->save($post);
-        // dd(route('posts.show', $post));
+        // dd($post->url);
 
         // When
-        $this->visit(route('posts.show', $post))
+        $this->visit($post->url)
             ->seeInElement('h1', $post->title)
             ->see($post->content)
             ->see($user->name);
+    }
+
+    function test_old_urls_are_redirected()
+    {
+        // Having
+        $user = $this->defaultUser();
+
+        $post = factory(\App\Post::class)->make([
+            'title' => 'Old title',
+        ]);
+
+        $user->posts()->save($post);
+
+        $url = $post->url;
+
+        $post->update(['title' => 'New title']);
+
+        // dd($url);
+        $this->visit($url)
+            ->seePageIs($post->url);
     }
 }
