a22113c98dce86b8927e5457c35c9ffb8da3b305
Josef <juarisco@outlook.com>
2019-11-17 16:33:56 -0500

    22.-Soporte-de-Markdown-en-Laravel-53-con-TDD

--- app/Post.php
+++ app/Post.php
@@ -4,6 +4,7 @@ namespace App;
 
 use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Model;
+use GrahamCampbell\Markdown\Facades\Markdown;
 
 class Post extends Model
 {
@@ -42,5 +43,10 @@ class Post extends Model
     public function getUrlAttribute()
     {
         return route('posts.show', [$this->id, $this->slug]);
+    }
+
+    public function getSafeHtmlContentAttribute()
+    {
+        return Markdown::convertToHtml(e($this->content));
     }
 }
