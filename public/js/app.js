0647bbe425fcc6d464330ef42d17903a5f30948a
Josef <juarisco@outlook.com>
2019-12-03 11:50:27 -0500

    64.-Uso-de-interceptores-con-axios-Laravel-y-Vue-js

--- public/js/app.js
+++ public/js/app.js
@@ -44984,7 +44984,22 @@ if (token) {
   window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
 } else {
   console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
-}
+} // Add a response interceptor
+
+
+axios.interceptors.response.use(function (response) {
+  // Any status code that lie within the range of 2xx cause this function to trigger
+  // Do something with response data
+  return response;
+}, function (error) {
+  // Any status codes that falls outside the range of 2xx cause this function to trigger
+  // Do something with response error
+  if (error.response.status == 401) {
+    window.location.replace('login');
+  } else {
+    return Promise.reject(error);
+  }
+});
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
  * for events that are broadcast by Laravel. Echo and event broadcasting
--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -11,7 +11,7 @@ try {
     window.$ = window.jQuery = require('jquery');
 
     require('bootstrap-sass');
-} catch (e) {}
+} catch (e) { }
 
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
@@ -36,6 +36,22 @@ if (token) {
 } else {
     console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
 }
+
+// Add a response interceptor
+axios.interceptors.response.use(function (response) {
+    // Any status code that lie within the range of 2xx cause this function to trigger
+    // Do something with response data
+    return response;
+}, function (error) {
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    // Do something with response error
+    if (error.response.status == 401) {
+        window.location.replace('login');
+    } else {
+        return Promise.reject(error);
+    }
+
+});
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
